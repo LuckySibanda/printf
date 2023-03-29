dc0eb4b4cb3fb1e4765cca696df8b8d850df451f
Lucky Sibanda <sibandalucky02@gmail.com>
2023-03-29 02:15:07 +0000

    fixed betty errors

--- main.h
+++ main.h
@@ -1,24 +1,24 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stddef.h>
 #include <stdarg.h>
 
 /**
- * struct print_fn - Struct to store format and corresponding print function
- * @format: The format specifier
- * @fn: The corresponding print function
- */
+
+struct print_fn - Struct print function
+@fmt: The format
+@fn: The function
+*/
 typedef struct print_fn
 {
-	char format;
-	int (*fn)(va_list);
+char *fmt;
+int (*fn)(va_list);
 } print_fn_t;
-
-/* Function prototypes */
-int _printf(const char *format, ...);
 int _putchar(char c);
-int print_char(va_list args);
-int print_string(va_list args);
+int _printf(const char *format, ...);
+int print_c(va_list args);
+int print_s(va_list args);
 int print_percent(va_list args);
 
 #endif /* MAIN_H */
