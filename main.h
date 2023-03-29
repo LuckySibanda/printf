02274d9997dc5fc7bf2b84ab64347c3dbb153063
Lucky Sibanda <sibandalucky02@gmail.com>
2023-03-29 00:28:50 +0000

    fixed main.h to comply with Betty style coding by sortening line length

--- main.h
+++ main.h
@@ -1,32 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
-
+#include <stddef.h>
 #include <stdarg.h>
 
 /**
- * struct print_fn - Struct containing a print function
+ * struct print_fn - Struct containing a print function and
  * and its associated format specifier
- * @specifier: The format specifier
+ * @fmt: The format specifier
  * @fn: The print function
  */
 typedef struct print_fn
 {
-	char *specifier;
-	int (*fn)(va_list);
+char *fmt;
+int (*fn)(va_list);
 } print_fn_t;
-
-/**
- * struct print_specifiers - Struct containing a format specifier
- * and its associated print function
- * @specifier: The format specifier
- * @print_fn: The print function associated with the format specifier
- */
-typedef struct print_specifiers
-{
-	char specifier;
-	int (*print_fn)(va_list);
-} print_specifiers_t;
-
+int _putchar(char c);
 int _printf(const char *format, ...);
+int print_c(va_list args);
+int print_s(va_list args);
+int print_percent(va_list args);
 
 #endif /* MAIN_H */
